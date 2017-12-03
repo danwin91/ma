10c55db5e32c5b2ce8b0baa594de4fd5760d6512
Michael Rauchensteiner <michael.rauchensteiner@iteratec.de>
2017-12-03 19:31:03 +0100

    done?

--- myplots.py
+++ myplots.py
@@ -16,3 +16,24 @@ def plot_singular_values(D, m, m_1, m_x, der, filename):
     plt.title(s=r'Singular values of $M_{}$ for $m={},m_1={}, m_x={}$'.format(der,m,m_1,m_x),  fontsize=16)
     plt.legend(loc='upper right')
     plt.savefig(filepath, format='pdf', dpi=1200)
+
+def plot_summary_dist(m_vec, y1, y1_label, y2, y2_label, y3, y3_label,title,  filename):
+    filepath = os.path.join(plotpath, filename)
+    plt.plot(m_vec, y1, "bx-", label=y1_label)
+    plt.plot(m_vec, y2, "rx-", label=y2_label)
+    plt.plot(m_vec, y3, "gx-", label=y3_label)
+    plt.xlabel(r"$m$")
+    plt.title(s=title,  fontsize=16)
+    plt.legend(loc='upper right')
+    plt.savefig(filepath, format='pdf', dpi=1200)
+
+
+def plot_summary_ratio(m_vec, y1, y1_label, y2, y2_label, title, filename):
+    filepath = os.path.join(plotpath, filename)
+    plt.plot(m_vec, y1, "bx-", label=y1_label)
+    plt.plot(m_vec, y2, "rx-", label=y2_label)
+    plt.xlabel(r"$m$")
+    plt.ylabel(r"$ratio(D_k, j)$")
+    plt.title(s=title,  fontsize=16)
+    plt.legend(loc='upper right')
+    plt.savefig(filepath, format='pdf', dpi=1200)
